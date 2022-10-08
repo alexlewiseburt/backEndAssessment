964640cf3f14fa6bcdd9a7289425a3a3e4c56092
Alexandrea Burt <alexlewiseburt@gmail.com>
2022-10-07 23:51:05 -0400

    adding fortune feature

--- server/controller.js
+++ server/controller.js
@@ -1,13 +1,30 @@
 module.exports = {
+  getCompliment: (req, res) => {
+    const compliments = [
+      "Gee, you're a smart cookie!",
+      "Cool shirt!",
+      "Your Javascript skills are stellar.",
+    ];
 
-    getCompliment: (req, res) => {
-        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
-      
-        // choose random compliment
-        let randomIndex = Math.floor(Math.random() * compliments.length);
-        let randomCompliment = compliments[randomIndex];
-      
-        res.status(200).send(randomCompliment);
-    }
+    // choose random compliment
+    let randomIndex = Math.floor(Math.random() * compliments.length);
+    let randomCompliment = compliments[randomIndex];
 
-}
+    res.status(200).send(randomCompliment);
+  },
+
+  getFortune: (req, res) => {
+    const fortunes = [
+      "A smooth long journey! Great expectations.",
+      "An inch of time is an inch of gold!",
+      "Any day above ground is a good day.",
+      "Be careful or you could fall for some tricks today.",
+      "Curiosity kills boredom. Nothing can kill curiosity.",
+    ];
+
+    let randomIndex = Math.floor(Math.random() * fortunes.length);
+    let randomFortune = fortunes[randomIndex];
+
+    res.status(200).send(randomFortune);
+  },
+};
